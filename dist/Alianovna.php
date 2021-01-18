a835affc2c71470cf52dc25edbbf9d1c0d69adf8
Anthony <anthony@aouka.com>
2021-01-19 00:11:00 +0100

    Numbers of keys and random bytes length are settable /// Registry content is crypted (with the given key) and splitted into multiple files /// the keypass for decrypt session are now uniq for each session (concat random token with the crypt given) /// it's possible to hard refresh (recreate all files and re-inject data) /// the random offload can now be setted in parameter /// etc...

--- dist/Alianovna.php
+++ dist/Alianovna.php
@@ -26,10 +26,15 @@ class Alianovna
 	const REGISTRY = 'registry';
 	const SESSION = 'session';
 
-	const SSL_RANDOM_LENGTH = 128;
-
-	const KEYS_NB = 3;
+	const DEFAULT_SSL_RANDOM_LENGTH = 128;
+	const DEFAULT_KEYS_NB = 3;
 	const KEYS_NAME = 'ALIANOVNA_KEY_';
+
+	/** @var int Number of keys / cookies required for bind and open session */
+	private $nbKeys = self::DEFAULT_KEYS_NB;
+
+	/** @var int Openssl random pseudo bytes length */
+	private $randomLength = self::DEFAULT_SSL_RANDOM_LENGTH;
 
 	/** @var int Cookie duration */
 	private $expire;
@@ -55,6 +60,9 @@ class Alianovna
 	/** @var string */
 	private $session = null;
 
+	/** @var string */
+	private $secret = null;
+
 	/** @var array */
 	private $data = null;
 
@@ -62,12 +70,13 @@ class Alianovna
 	 * Alias encrypt
 	 *
 	 * @param string $text
+	 * @param string $crypt
 	 * @return string
 	 */
-	private function encrypt(string $text): string
+	private function encrypt(string $text, string $crypt): string
 	{
 		try {
-			return Crypt::encrypt($text, Crypt::createNewKey($this->crypt));
+			return Crypt::encrypt($text, Crypt::createNewKey($crypt));
 		}
 		catch (Exception $e) {
 			return '';
@@ -78,12 +87,13 @@ class Alianovna
 	 * Alias decrypt
 	 *
 	 * @param string $cipher
+	 * @param string $crypt
 	 * @return string
 	 */
-	private function decrypt(string $cipher)
+	private function decrypt(string $cipher, string $crypt)
 	{
 		try {
-			return Crypt::decrypt($cipher, Crypt::createNewKey($this->crypt));
+			return Crypt::decrypt($cipher, Crypt::createNewKey($crypt));
 		}
 		catch (Exception $e) {
 			return '';
@@ -97,30 +107,50 @@ class Alianovna
 	 */
 	private function token(): string
 	{
-		return hash('sha512', openssl_random_pseudo_bytes(self::SSL_RANDOM_LENGTH), false);
+		return hash('sha512', openssl_random_pseudo_bytes($this->randomLength), false);
 	}
 
 	/**
-	 * Load keys from customer cookkies
+	 * Load keys from customer cookies
 	 *
 	 * @return bool
 	 */
 	private function loadKeys(): bool
 	{
 		if(!$this->keys) {
-			foreach (range(1,self::KEYS_NB) as $k) {
+			foreach (range(1, $this->nbKeys) as $k) {
 				$key = self::KEYS_NAME . $k;
 				$registry = $this->cookie->getSafe($key);
 				if($registry) {
 					$this->keys[$key] = $registry;
 				}
 			}
-			if(count($this->keys) !== self::KEYS_NB) {
+			if(count($this->keys) !== $this->nbKeys) {
 				$this->keys = [];
 				return false;
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * Create registry content
+	 *
+	 * @return array
+	 */
+	private function createRegistryData(): array
+	{
+		$encrypted = $this->encrypt(json_encode([
+			'secret' => $this->secret,
+			'session' => $this->session
+		]), $this->crypt);
+
+		$length = round(strlen($encrypted) / $this->nbKeys);
+		$chunks = str_split($encrypted, $length);
+
+		array_unshift($chunks,'');
+		unset($chunks[0]);
+		return $chunks;
 	}
 
 	/**
@@ -140,6 +170,7 @@ class Alianovna
 		} while(true);
 		$this->data = [];
 		$this->session = $session;
+		$this->secret = $this->token() . $this->crypt;
 		return $this->write($path, '');
 	}
 
@@ -151,8 +182,16 @@ class Alianovna
 	 */
 	private function createRegistyFiles(): bool
 	{
+		$this->keys = [];
+		$datas = $this->createRegistryData();
+		$range = range(1, $this->nbKeys);
+		$keys = array_keys($datas);
+		if(count($range) !== count($keys) || array_diff($range, $keys)) {
+			return false;
+		}
+
 		$status = true;
-		foreach (range(1,self::KEYS_NB) as $k) {
+		foreach ($range as $k) {
 			$key = self::KEYS_NAME . $k;
 			do {
 				$registry = $this->token();
@@ -162,7 +201,7 @@ class Alianovna
 				}
 			} while(true);
 			$this->keys[$key] = $registry;
-			$written = $this->write($path, $this->session);
+			$written = $this->write($path, $datas[$k]);
 			$cooked = $this->cookie->setSafe($key, $registry, time() + $this->expire);
 			$status = $status && $written && $cooked;
 		}
@@ -172,53 +211,51 @@ class Alianovna
 	/**
 	 * Delete all registry files and cookies
 	 *
-	 * @return array List of sessions token contains in registry keys
+	 * @return string List of sessions token contains in registry keys
 	 */
-	private function deleteRegistryFiles(): array
+	private function deleteRegistryFiles(): string
 	{
-		$sessions = [];
-		foreach (range(1,self::KEYS_NB) as $k) {
+		$session = '';
+		foreach (range(1, $this->nbKeys) as $k) {
 			$key = self::KEYS_NAME . $k;
 			$registry = $this->keys[$key] ?? '';
 			if(!$registry) {
 				$registry = $this->cookie->getSafe($key);
 			}
 			$path = $this->registryDir . DIRECTORY_SEPARATOR . $registry;
-			if(!$this->session && is_file($path) && $session = file_get_contents($path)) {
-				$sessions[$session] = $session;
+			if(!$this->session && is_file($path) && $chunk = file_get_contents($path)) {
+				$session .= $chunk;
 			}
 			if(is_file($path)) {
 				unlink($path);
 			}
 			$this->cookie->delete(self::KEYS_NAME . $k);
 		}
-		return $sessions;
+		$decrypted = $this->decrypt($session, $this->crypt);
+		$data = $decrypted ? json_decode($decrypted) : null;
+		return strval($data->session ?? '');
 	}
 
 	/**
 	 * Delete session file
 	 *
-	 * @param array $sessions [optional]
+	 * @param string $session [optional]
 	 * @return bool
 	 */
-	private function deleteSessionFile(array $sessions = []): bool
+	private function deleteSessionFile(string $session = ''): bool
 	{
-		if($sessions) {
-			foreach ($sessions as $k) {
-				$path = $this->sessionDir . DIRECTORY_SEPARATOR . $k;
-				if(is_file($path)) {
-					unlink($path);
-				}
+		if($session) {
+			$path = $this->sessionDir . DIRECTORY_SEPARATOR . $session;
+			if(is_file($path)) {
+				unlink($path);
 			}
 		}
-
 		if($this->session) {
 			$path = $this->sessionDir . DIRECTORY_SEPARATOR . $this->session;
 			if(is_file($path)) {
 				unlink($path);
 			}
 		}
-
 		return true;
 	}
 
@@ -321,6 +358,34 @@ class Alianovna
 	}
 
 	/**
+	 * Openssl random pseudo bytes length
+	 *
+	 * @param int $length [optional]
+	 * @return $this
+	 */
+	public function randomLength(int $length = self::DEFAULT_SSL_RANDOM_LENGTH): Alianovna
+	{
+		if($length >= self::DEFAULT_SSL_RANDOM_LENGTH) {
+			$this->randomLength = $length;
+		}
+		return $this;
+	}
+
+	/**
+	 * Number of keys / cookies required for bind and open session
+	 *
+	 * @param int $nb [optional]
+	 * @return $this
+	 */
+	public function nbKeys(int $nb = self::DEFAULT_KEYS_NB): Alianovna
+	{
+		if($nb >= 1) {
+			$this->nbKeys = $nb;
+		}
+		return $this;
+	}
+
+	/**
 	 * Generate customer session
 	 *
 	 * @return bool
@@ -340,22 +405,29 @@ class Alianovna
 	/**
 	 * Refresh registry tokens
 	 *
+	 * @param bool $hard [optional]
 	 * @return bool
 	 * @throws Exception
 	 */
-	public function refresh(): bool
+	public function refresh(bool $hard = false): bool
 	{
-		if(!$this->loadKeys() || !$this->session) {
+		if($hard) {
+			$saved = true;
+			$data = $this->data;
 			$this->destroy();
-			return false;
+			$created = $this->create();
+			if($data) {
+				$saved = $this->save($data);
+			}
+			return $created && $saved;
 		}
-
-		$this->deleteRegistryFiles();
-		if(!$this->createRegistyFiles()) {
-			return false;
+		else {
+			if(!$this->session || !$this->secret) {
+				return false;
+			}
+			$this->deleteRegistryFiles();
+			return $this->createRegistyFiles();
 		}
-
-		return true;
 	}
 
 	/**
@@ -371,31 +443,30 @@ class Alianovna
 			return false;
 		}
 
-		if(!$this->session) {
-			$file = null;
+		if(!$this->session || !$this->secret) {
+			$raw = '';
 			foreach ($this->keys as $registry) {
 				$path = $this->registryDir . DIRECTORY_SEPARATOR . $registry;
-				if(!is_file($path)) {
+				if(!is_file($path) || !($chunk = file_get_contents($path))) {
 					return false;
 				}
-				$session = file_get_contents($path);
-				if(!$session) {
-					return false;
+				$raw .= $chunk;
+			}
+			if($raw && $json = $this->decrypt($raw, $this->crypt)) {
+				$params = json_decode($json);
+				if($session = $params->session ?? null) {
+					$this->session = $session;
 				}
-				elseif(null === $file) {
-					$file = $session;
-				}
-				elseif($file !== $session) {
-					return false;
+				if($secret = $params->secret ?? null) {
+					$this->secret = $secret;
 				}
 			}
-			$this->session = $file;
 		}
 
 		$path = $this->sessionDir . DIRECTORY_SEPARATOR . $this->session;
-		if($this->session && is_file($path)) {
+		if($this->session && $this->secret && is_file($path)) {
 			$raw = file_get_contents($path);
-			if($raw && $serialized = $this->decrypt($raw)) {
+			if($raw && $serialized = $this->decrypt($raw, $this->secret)) {
 				$this->data = unserialize($serialized);
 			}
 			elseif(null === $raw) {
@@ -424,6 +495,7 @@ class Alianovna
 
 		$this->keys = [];
 		$this->session = null;
+		$this->secret = null;
 		$this->data = null;
 		return $this;
 	}
@@ -438,14 +510,14 @@ class Alianovna
 	public function save(array $overwrite = null): bool
 	{
 		$path = $this->sessionDir . DIRECTORY_SEPARATOR . $this->session;
-		if(!$this->session || !is_file($path)) {
+		if(!$this->session || !$this->secret || !is_file($path)) {
 			return false;
 		}
 		if(null !== $overwrite) {
 			$this->data = $overwrite;
 		}
 		$serialized = serialize($this->data);
-		$encrypted = $this->encrypt($serialized);
+		$encrypted = $this->encrypt($serialized, $this->secret);
 		return $this->write($path, $encrypted);
 	}
 
@@ -500,14 +572,14 @@ class Alianovna
 
 	/**
 	 * Offload expired sessions.
-	 * (one chance in 100)
 	 *
+	 * @param int $rand [optional]
 	 * @param int $expire [optional]
 	 * @return $this
 	 */
-	public function offload(int $expire = null): Alianovna
+	public function offload(int $rand = 0, int $expire = null): Alianovna
 	{
-		if(!rand(0,99)) {
+		if(!rand(0, abs($rand))) {
 			$now = time();
 			if(null === $expire) {
 				$expire = $this->expire;
